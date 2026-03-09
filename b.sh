f780f62fcb7d574e06146d0e7dcf49b13215afdb
vulcannodes97-sudo <vulcannodes97@gmail.com>
2026-03-10 01:06:49 +0530

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-cd /var/www/pterodactyl || exit
+echo "===== Pterodactyl User Admin Editor ====="
 
-# auto detect panel domain from nginx
+# Auto detect panel domain from nginx
 DOMAIN=$(grep server_name /etc/nginx/sites-available/pterodactyl.conf | awk '{print $2}' | sed 's/;//')
-
 PANEL_URL="https://$DOMAIN"
 
-# read API key
+echo "Detected Panel URL: $PANEL_URL"
+
+# API key
 read -p "Enter Application API Key: " API_KEY
 
-echo "Panel detected: $PANEL_URL"
-
+# install jq if missing
 apt install jq -y >/dev/null 2>&1
 
+echo ""
 echo "Fetching users..."
+echo ""
 
-USERS=$(curl -s "$PANEL_URL/api/application/users" \
+curl -s "$PANEL_URL/api/application/users" \
 -H "Authorization: Bearer $API_KEY" \
--H "Accept: Application/vnd.pterodactyl.v1+json")
-
-echo "$USERS" | jq -r '.data[].attributes.id' | while read ID
+-H "Accept: Application/vnd.pterodactyl.v1+json" | jq -c '.data[]' | while read user
 do
-echo "Setting admin for user ID $ID"
+
+ID=$(echo $user | jq -r '.attributes.id')
+USERNAME=$(echo $user | jq -r '.attributes.username')
+EMAIL=$(echo $user | jq -r '.attributes.email')
+
+echo "User Found → $USERNAME ($EMAIL)"
+
+echo "Making admin..."
 
 curl -s -X PATCH "$PANEL_URL/api/application/users/$ID" \
 -H "Authorization: Bearer $API_KEY" \
@@ -30,8 +37,9 @@ curl -s -X PATCH "$PANEL_URL/api/application/users/$ID" \
 -H "Accept: Application/vnd.pterodactyl.v1+json" \
 -d '{"root_admin": true}' >/dev/null
 
+echo "Admin Enabled"
+echo "---------------------------"
+
 done
 
-echo "--------------------------------"
-echo "All users are now ADMIN"
-echo "--------------------------------"
+echo "All users processed."

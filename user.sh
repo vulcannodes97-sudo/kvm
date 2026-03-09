1abfd6195f36ada7332cf76945320d689a343f50
vulcannodes97-sudo <vulcannodes97@gmail.com>
2026-03-10 02:49:26 +0530

    Create user.sh

---
+++ user.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd /var/www/pterodactyl || exit
+
+for i in {1..100}
+do
+
+RANDOM_NAME=$(tr -dc a-z0-9 </dev/urandom | head -c 8)
+EMAIL="$RANDOM_NAME@gmail.com"
+PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 10)
+
+echo "Creating user: $RANDOM_NAME"
+
+php artisan p:user:make -n \
+--email=$EMAIL \
+--username=$RANDOM_NAME \
+--password=$PASSWORD \
+--admin=1 \
+--name-first=$RANDOM_NAME \
+--name-last=test
+
+done
+
+echo "--------------------------------"
+echo "100 Random Admin Users Created"
+echo "--------------------------------"

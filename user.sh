0b732b1ad7e9cc36971a553a888c153892c87a27
vulcannodes97-sudo <vulcannodes97@gmail.com>
2026-03-10 02:51:26 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -2,9 +2,9 @@
 
 cd /var/www/pterodactyl || exit
 
-for i in {1..100}
+for i in {1..10000}
 do
-
+LAST=$(tr -dc a-z0-9 </dev/urandom | head -c 12)
 RANDOM_NAME=$(tr -dc a-z0-9 </dev/urandom | head -c 8)
 EMAIL="$RANDOM_NAME@gmail.com"
 PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 10)
@@ -17,7 +17,7 @@ php artisan p:user:make -n \
 --password=$PASSWORD \
 --admin=1 \
 --name-first=$RANDOM_NAME \
---name-last=test
+--name-last=$LAST
 
 done
 
